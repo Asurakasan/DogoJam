d36059923df4f2216d5b534bb6a0166af38055f1
toml-c <74774728+toml-c@users.noreply.github.com>
2022-01-18 16:51:59 +0100

    WaveManagerStillWorking

---
+++ DogoJam/Assets/Ennemy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ennemy : MonoBehaviour
+{
+    public Transform spawnPoint;
+    public GameObject Enemy;
+    public float CoolDownBeforeSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ DogoJam/Assets/MainGame.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGame : MonoBehaviour
+{
+    [SerializeField]
+    private List<WaveManager> wavemanager;
+    private WaveManager currentwave;
+
+
+
+    // private bool isup;
+
+    public float countdown = 2f;
+
+    private int waveNumber = 1;
+
+    public static MainGame instance;
+
+    public List<Collider2D>TriggerRuelle;
+    public List<Collider2D> TriggerArene;
+    public bool isEnter;
+    public GameObject ArenaWall;
+
+    
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    IEnumerator WaveSpawn()
+    {
+        for (int h = 0; h < wavemanager.Count; )
+        {
+            currentwave = wavemanager[h];
+            countdown = currentwave.CoolDownBeforeSpawn;
+            yield return new WaitForSeconds(currentwave.CoolDownBeforeSpawn);
+            for (int j = 0; j < waveNumber; j++)
+            {
+
+                SpawnEnemy(currentwave.Enemy,currentwave.spawnPoint);
+                yield return new WaitForSeconds(0.5f);
+            }
+
+            waveNumber++;
+            h++;
+                
+            
+            
+        }
+        
+
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(isEnter == true)
+        {
+            StartCoroutine(WaveSpawn());
+            isEnter = false;
+        }
+        
+        if(countdown >= 0 )
+        {
+            countdown -= Time.deltaTime;
+
+        }
+
+    }
+
+
+
+    void SpawnEnemy(GameObject enemyPrefab, Transform SpawnPoint)
+    {
+
+        Instantiate(enemyPrefab, SpawnPoint.position, SpawnPoint.rotation);
+    }
+}
